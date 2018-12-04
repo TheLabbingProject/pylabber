b7ba587724b81733d4ba76c7e53a296cb8c1c2fa
Zvi Baratz <z.baratz@gmail.com>
2018-12-04 09:36:42 +0200

    Added user profile image field and improved home template

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+from accounts.choices import Title
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
@@ -10,7 +11,19 @@ class User(AbstractUser):
 
 class Profile(models.Model):
     user = models.OneToOneField(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='profile',
+    )
+    image = models.ImageField(
+        upload_to='images',
+        blank=True,
+    )
+    title = models.CharField(
+        max_length=20,
+        choices=Title.choices(),
+        default=Title.NONE,
+    )
     date_of_birth = models.DateField(default=None, blank=True, null=True)
     institute = models.CharField(max_length=255, blank=True)
     bio = models.TextField(max_length=500, blank=True)
@@ -20,3 +33,12 @@ class Profile(models.Model):
 
     def get_absolute_url(self):
         return reverse('user_detail', args=[str(self.user.id)])
+
+    def get_full_name(self):
+        full_name = f'{self.user.first_name} {self.user.last_name}'
+        if self.title:
+            return full_name + f', {Title[self.title].value}.'
+        return full_name
+
+    def get_study_list(self):
+        return [study.name for study in self.user.studies.all()]

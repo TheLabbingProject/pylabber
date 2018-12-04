585b749099a2f6b25c5e6e9583483507f9829a67
Zvi Baratz <z.baratz@gmail.com>
2018-12-04 12:35:52 +0200

    Improved home template to display researcher's grid

--- accounts/choices.py
+++ accounts/choices.py
@@ -3,6 +3,17 @@ from pylabber.utils import ChoiceEnum
 
 class Title(ChoiceEnum):
     NONE = ''
-    MSC = 'MSc'
-    PHD = 'PhD'
-    PROF = 'Prof'
+    BSC = 'B.Sc.'
+    MSC = 'M.Sc.'
+    PHD = 'Ph.D.'
+    PROF = 'Prof.'
+
+
+class Position(ChoiceEnum):
+    NONE = ''
+    RA = 'Research Assistant'
+    MSC = 'M.Sc. Student'
+    PHD = 'Ph.D. Candidate'
+    POST = 'Postdoctoral Researcher'
+    MAN = 'Lab Manager'
+    HEAD = 'Principle Investigator'
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
-from django.conf import settings
 from django.urls import path
 
 from . import views
+
+app_name = 'accounts'
 
 urlpatterns = [
     path(

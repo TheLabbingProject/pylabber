9e549965f45413fb6f14dd1072c354df63e0d48c
Zvi Baratz <z.baratz@gmail.com>
2019-02-07 15:21:49 +0200

    Tiny commit

--- pylabber/celery.py
+++ pylabber/celery.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
@@ -7,3 +8,8 @@ app = Celery("pylabber")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print("Request: {0!r}".format(self.request))

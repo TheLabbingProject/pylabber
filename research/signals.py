6130205b0feb7b60c1e9199ce68dd82d7beef5ba
Zvi Baratz <z.baratz@gmail.com>
2019-03-19 15:18:43 +0200

    Working on fixing subject association with DICOM patients

--- research/signals.py
+++ research/signals.py
@@ -1,3 +1,4 @@
+from django.db.models import ObjectDoesNotExist
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from research.models.subject import Subject
@@ -5,6 +6,7 @@ from research.models.subject import Subject
 
 @receiver(post_save, sender="django_dicom.Patient")
 def save_subject(sender, instance, **kwargs):
+    # TODO: Fix subject association with patient, currently causing error when instantiating new subject
     existing_ids = Subject.objects.values_list("id_number", flat=True).all()
     if instance.patient_id not in existing_ids:
         subject_attributes = instance.get_subject_attributes()

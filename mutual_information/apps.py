e7a3d253b1f36fbf03538c8428819a911e88e0d0
Zvi Baratz <z.baratz@gmail.com>
2019-08-20 13:37:49 +0300

    Deleted old bokeh integration and mutual information calculation directories

--- mutual_information/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MutualInformationConfig(AppConfig):
-    name = 'mutual_information'
--- mutual_information/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from mutual_information.models.mutual_information_score import MutualInformationScore

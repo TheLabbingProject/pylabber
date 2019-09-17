3f93b385d1467a9826ad84d28515babd3526ff03
Zvi Baratz <z.baratz@gmail.com>
2019-09-17 18:56:46 +0300

    Changed LabMembership to LaboratoryMembership and 'Position' Enum to 'Role' (together with the corresponding attribute for LaboratoryMembership)

--- accounts/models/__init__.py
+++ accounts/models/__init__.py
@@ -1,4 +1,4 @@
 from accounts.models.user import User
 from accounts.models.laboratory import Laboratory
 from accounts.models.profile import Profile
-from accounts.models.lab_membership import LabMembership
+from accounts.models.lab_membership import LaboratoryMembership

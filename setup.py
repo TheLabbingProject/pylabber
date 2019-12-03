950f2b1b5213c06b87aeb0defc3eb5dbd2479f01
Zvi Baratz <z.baratz@gmail.com>
2019-12-03 18:01:19 +0200

    Updated requirements

--- setup.py
+++ setup.py
@@ -7,36 +7,38 @@ with open("README.md", "r") as fh:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-with open("requirements/common.txt") as fh:
+with open("requirements.txt") as fh:
     requirements = fh.read().splitlines()
 
-with open("requirements/dev.txt") as fh:
+with open("requirements-dev.txt") as fh:
     dev_requirements = fh.read().splitlines()
 
 setup(
     name="pylabber",
-    version="0.1.0",
+    version="0.0.2",
     packages=find_packages(),
     include_package_data=True,
     scripts=["manage.py"],
-    license="MIT",
+    license="AGPLv3",
     description="A Django app to manage neuroscience research",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ZviBaratz/pylabber",
+    url="https://github.com/TheLabbingProject/pylabber",
     author="Zvi Baratz",
-    author_email="z.baratz@gmail.com",
-    keywords="django research neuroscience mri analysis",
+    author_email="baratzz@pm.me",
+    keywords="django research neuroscience",
+    python_requires=">=3.6",
     install_requires=requirements,
     extras_require={"dev": dev_requirements},
     classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
         "Environment :: Web Environment",
-        "Framework :: Django",
-        "Framework :: Django :: 2.1",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
+        "Framework :: Django :: 2.2",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent",
-        "Programming Language :: Python",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )

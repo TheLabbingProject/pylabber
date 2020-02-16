4e9113b71a2421dc25e6da911a447888c92516ba
Noam Aharony <aharonyn@gmail.com>
2020-02-16 12:07:44 +0200

    userSerializer now has a profile field, that contains the related object. In order to fix userEdit in frontend.

--- accounts/serializers/user.py
+++ accounts/serializers/user.py
@@ -2,31 +2,23 @@ from accounts.models.profile import Profile
 from accounts.models.user import User
 from rest_framework import serializers
 from rest_auth.serializers import UserDetailsSerializer
+from accounts.serializers.profile import ProfileSerializer
 
 
 class UserSerializer(UserDetailsSerializer):
     """
     `Serializer <https://www.django-rest-framework.org/api-guide/serializers/>`_
     class for the :class:`~accounts.models.user.User` model.
-    
     """
 
     url = serializers.HyperlinkedIdentityField(view_name="accounts:user-detail")
-    image = serializers.ImageField(source="profile.image")
-    title = serializers.CharField(source="profile.title")
-    date_of_birth = serializers.DateField(source="profile.date_of_birth")
-    institute = serializers.CharField(source="profile.institute")
-    bio = serializers.CharField(source="profile.bio", allow_blank=True)
+    profile = ProfileSerializer()
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
             "url",
             "id",
-            "image",
-            "title",
-            "date_of_birth",
-            "institute",
-            "bio",
+            "profile",
             "is_staff",
             "laboratory_set",
         )

d7890ee21c0a424964dbfca5a716b783bfd9fd71
Noam Aharony <aharonyn@gmail.com>
2020-07-28 16:32:22 +0300

    Added Task model, view and serializer. Starting with measurement support.

--- research/models/__init__.py
+++ research/models/__init__.py
@@ -9,3 +9,4 @@ between the different models, see the :ref:`overview:Overview`.
 from research.models.group import Group
 from research.models.study import Study
 from research.models.subject import Subject
+from research.models.task import Task
---
+++ research/models/task.py
@@ -0,0 +1,14 @@
+"""
+Definition of the :class:`~research.models.task.Task` model.
+"""
+
+from research.models.event import Event
+
+
+class Task(Event):
+    """
+    Represents an experimental task.
+    """
+
+    pass
+

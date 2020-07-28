6250702127ea2301659c8ecd54fee4ebf6407c52
Zvi Baratz <z.baratz@gmail.com>
2020-07-29 00:44:31 +0300

    Added AWS_REGION_NAME setting.

--- pylabber/settings.py
+++ pylabber/settings.py
@@ -172,8 +172,9 @@ if os.getenv("USE_S3"):
     AWS_DEFAULT_ACL = None
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
-    AWS_LOCATION = "static"
+    AWS_S3_REGION_NAME = "eu-central-1"
     # s3 static settings
+    # AWS_LOCATION = "static"
     # STATIC_LOCATION = "static"
     # STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/"
     # STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"

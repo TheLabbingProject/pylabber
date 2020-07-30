a544da51d2d00cf559a244809c2fb07dd0d74b02
Zvi Baratz <z.baratz@gmail.com>
2020-07-30 15:28:11 +0300

    Changed S3Boto3Storage subclass in storage backends to include a default ACL value of 'public-read' after getting 403 from AWS in deployment.

--- pylabber/storage_backends.py
+++ pylabber/storage_backends.py
@@ -1,6 +1,8 @@
+from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class MediaStorage(S3Boto3Storage):
-    location = "media"
+    location = settings.PUBLIC_MEDIA_LOCATION
     file_overwrite = False
+    default_acl = "public-read"

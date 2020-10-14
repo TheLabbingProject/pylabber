2cfd12555d09e54a23a7a0ab1804b5e5b7083402
Noam Aharony <aharonyn@gmail.com>
2020-10-14 13:59:59 +0300

    Added views and serializers to the new models. Fixed and updated tests according to the ViewSet concept. Wrote new tests for the new models.

--- research/models/event.py
+++ research/models/event.py
@@ -4,6 +4,7 @@ Definition of the :class:`~research.models.event.Event` model.
 
 from django_extensions.db.models import TitleDescriptionModel
 from model_utils.managers import InheritanceManager
+from django.urls import reverse
 
 
 class Event(TitleDescriptionModel):
@@ -27,3 +28,22 @@ class Event(TitleDescriptionModel):
         """
 
         return f"{self.title}|{self.description}"
+
+    def get_absolute_url(self):
+        """
+        Returns the canonical URL for this instance.
+
+        References
+        ----------
+        * `get_absolute_url()`_
+
+        .. _get_absolute_url():
+            https://docs.djangoproject.com/en/3.0/ref/models/instances/#get-absolute-url
+
+        Returns
+        -------
+        str
+            URL
+        """
+
+        return reverse("research:event-detail", args=[str(self.id)])
--- research/models/task.py
+++ research/models/task.py
@@ -3,6 +3,7 @@ Definition of the :class:`~research.models.task.Task` model.
 """
 
 from research.models.event import Event
+from django.urls import reverse
 
 
 class Task(Event):
@@ -10,5 +11,22 @@ class Task(Event):
     Represents an experimental task.
     """
 
-    pass
+    def get_absolute_url(self):
+        """
+        Returns the canonical URL for this instance.
+
+        References
+        ----------
+        * `get_absolute_url()`_
+
+        .. _get_absolute_url():
+            https://docs.djangoproject.com/en/3.0/ref/models/instances/#get-absolute-url
+
+        Returns
+        -------
+        str
+            URL
+        """
+
+        return reverse("research:task-detail", args=[str(self.id)])
 

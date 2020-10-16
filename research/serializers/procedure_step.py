047be853aced09dd1c364c98ff753528fc11a0a9
Zvi Baratz <z.baratz@gmail.com>
2020-10-16 13:30:08 +0300

    Update procedure_step.py

--- research/serializers/procedure_step.py
+++ research/serializers/procedure_step.py
@@ -1,5 +1,5 @@
 """
-Definition of the :class:`~ProcedureStepSerializer` class.
+Definition of the :class:`ProcedureStepSerializer` class.
 """
 
 from research.models.procedure_step import ProcedureStep

d7f6e87768a1ae280abc019abe340dd805f15db3
Zvi Baratz <z.baratz@gmail.com>
2020-11-15 19:33:03 +0200

    Added basic admin pages for Procedure, MeasurementDefinition, and Task.

--- research/admin.py
+++ research/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
+from research.models.event import Event
+from research.models.measurement_definition import MeasurementDefinition
+from research.models.procedure import Procedure
+from research.models.procedure_step import ProcedureStep
 from research.models.study import Study
 from research.models.subject import Subject
+from research.models.task import Task
 
 
 class SubjectsInline(admin.TabularInline):
     model = Study.subjects.through
     verbose_name_plural = "Subjects"
-    readonly_fields = ("id_number", "first_name", "last_name", "sex", "date_of_birth")
+    readonly_fields = (
+        "id_number",
+        "first_name",
+        "last_name",
+        "sex",
+        "date_of_birth",
+    )
 
     def id_number(self, instance):
         return instance.subject.id_number
@@ -48,9 +59,71 @@ class SubjectAdmin(admin.ModelAdmin):
     list_filter = ("sex",)
 
     def changelist_view(self, request, extra_context=None):
-        response = super().changelist_view(request, extra_context=extra_context)
+        response = super().changelist_view(
+            request, extra_context=extra_context
+        )
         return response
 
 
+class ProcedureStepInline(admin.TabularInline):
+    model = ProcedureStep
+    extra = 0
+    fields = (
+        "index",
+        "event_type",
+        "event_title",
+        "event_description",
+    )
+    readonly_fields = (
+        "index",
+        "event_type",
+        "event_title",
+        "event_description",
+    )
+
+    def event_type(self, instance) -> str:
+        event = Event.objects.select_subclasses().get(id=instance.id)
+        event_type = event.__class__.__name__
+        return (
+            event_type
+            if event_type != "MeasurementDefinition"
+            else "Measurement"
+        )
+
+    def event_title(self, instance) -> str:
+        return instance.event.title
+
+    def event_description(self, instance) -> str:
+        return instance.event.description
+
+
+class ProcedureAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "description",
+    )
+    inlines = (ProcedureStepInline,)
+
+
+class MeasurementDefinitionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "description",
+    )
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "description",
+    )
+
+
+admin.site.register(MeasurementDefinition, MeasurementDefinitionAdmin)
+admin.site.register(Procedure, ProcedureAdmin)
 admin.site.register(Study, StudiesAdmin)
 admin.site.register(Subject, SubjectAdmin)
+admin.site.register(Task, TaskAdmin)

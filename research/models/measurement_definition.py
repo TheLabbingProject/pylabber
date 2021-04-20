e4b7d52f236adae97512135f2014183a559cbfde
Zvi Baratz <z.baratz@gmail.com>
2021-04-20 18:45:48 +0300

    Added the instance_set property to easily access associated measurements, resolves #105.

--- research/models/measurement_definition.py
+++ research/models/measurement_definition.py
@@ -35,3 +35,33 @@ class MeasurementDefinition(Event):
         """
 
         return reverse("research:measurement-detail", args=[str(self.id)])
+
+    def get_instance_set(self) -> models.QuerySet:
+        """
+        Returns a queryset of collected measurements associated with this
+        measurements definition.
+
+        Returns
+        -------
+        models.QuerySet
+            Collected data instances
+        """
+        DataModel = self.content_type.model_class()
+        return DataModel.objects.filter(measurement=self)
+
+    @property
+    def instance_set(self) -> models.QuerySet:
+        """
+        Returns a queryset of collected measurements associated with this
+        measurements definition.
+
+        Returns
+        -------
+        models.QuerySet
+            Collected data instances
+
+        See Also
+        --------
+        * :meth:`get_instance_set`
+        """
+        return self.get_instance_set()

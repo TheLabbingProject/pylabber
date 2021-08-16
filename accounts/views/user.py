64b60762dd87af1aaf0632ded9718fd99ad80b6c
Zvi Baratz <z.baratz@gmail.com>
2021-08-16 15:12:59 +0300

    User queryset filtering, resolves #145.

--- accounts/views/user.py
+++ accounts/views/user.py
@@ -1,26 +1,48 @@
 """
-Definition of the :class:`~accounts.views.user.UserViewSet` class.
+Definition of the :class:`UserViewSet` class.
 """
-
 from accounts.filters.user import UserFilter
 from accounts.serializers.user import UserSerializer
 from django.contrib.auth import get_user_model
+from django.db.models import QuerySet
 from pylabber.views.defaults import DefaultsMixin
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+
+User = get_user_model()
 
 
 class UserViewSet(DefaultsMixin, viewsets.ModelViewSet):
     """
     API endpoint that allows :class:`~accounts.models.user.User` instances to
     be viewed or edited.
-
     """
 
     filter_class = UserFilter
-    queryset = get_user_model().objects.all().order_by("date_joined")
+    queryset = User.objects.all().order_by("date_joined")
     serializer_class = UserSerializer
+
+    def filter_queryset(self, queryset) -> QuerySet:
+        """
+        Filter the returned users according to the requesting user's
+        permissions.
+
+        Parameters
+        ----------
+        queryset : QuerySet
+            Base queryset
+
+        Returns
+        -------
+        QuerySet
+            User instances
+        """
+        user = self.request.user
+        queryset = super().filter_queryset(queryset)
+        if user.is_staff or user.is_superuser:
+            return queryset
+        return queryset.filter(laboratory__in=user.laboratory_set.all())
 
     @action(detail=False, methods=["get"])
     def get_institutions(self, request):

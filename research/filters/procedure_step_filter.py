232c10985ea5ec3aadeb66d014e7d0c237a0f880
Zvi Baratz <z.baratz@gmail.com>
2021-10-18 21:01:54 +0300

    Added event type filter.

--- research/filters/procedure_step_filter.py
+++ research/filters/procedure_step_filter.py
@@ -1,10 +1,13 @@
 """
 Definition of the :class:`ProcedureStepFilter` class.
 """
+from django.db.models import QuerySet
 from django_filters import rest_framework as filters
 from research.filters.utils import LOOKUP_CHOICES
 from research.models.procedure import Procedure
 from research.models.procedure_step import ProcedureStep
+
+EVENT_TYPE_QUERY_KEY: str = "event__{event_type}__isnull"
 
 
 class ProcedureStepFilter(filters.FilterSet):
@@ -22,6 +25,14 @@ class ProcedureStepFilter(filters.FilterSet):
     exclude_procedure = filters.ModelMultipleChoiceFilter(
         field_name="procedure", exclude=True, queryset=Procedure.objects.all()
     )
+    event_type = filters.ChoiceFilter(
+        choices=(
+            ("measurementdefinition", "Measurement Definition"),
+            ("task", "Task"),
+        ),
+        method="check_event_type",
+        label="Event type:",
+    )
 
     class Meta:
         model = ProcedureStep
@@ -33,3 +44,9 @@ class ProcedureStepFilter(filters.FilterSet):
             "title",
             "description",
         )
+
+    def check_event_type(
+        self, queryset: QuerySet, name: str, value: str, *args, **kwargs
+    ) -> QuerySet:
+        key = EVENT_TYPE_QUERY_KEY.format(event_type=value)
+        return queryset.filter(**{key: False})

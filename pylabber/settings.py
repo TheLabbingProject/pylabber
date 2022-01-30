d164c903eda231dd40c0d05ba1559dbe69fcd66e
Zvi Baratz <z.baratz@gmail.com>
2022-01-30 14:57:57 +0200

    Added SSL settings.

--- pylabber/settings.py
+++ pylabber/settings.py
@@ -338,8 +338,11 @@ CORS_ORIGIN_WHITELIST = [
     "https://vuelabber.herokuapp.com",
     "https://pylabber.herokuapp.com",
 ]
-
-INTERNAL_IPS = ["127.0.0.1", "localhost"]
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+`INTERNAL_IPS = ["127.0.0.1", "localhost"]
 
 # research
 SUBJECT_MODEL = "research.Subject"
